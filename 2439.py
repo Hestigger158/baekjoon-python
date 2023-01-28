2e6e891af896d4aabc66ae57d50a4e8891b22199
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-01-28 13:41:56 +0900

    Create 2439.py

---
+++ 2439.py
@@ -0,0 +1,8 @@
+# baekjoon-python
+learn baekjoon python by my self.
+
+#은 수식 설명
+
+n = int(input())
+for i in range(n):
+    print(str('*'*(i+1)).rjust(n)) # rjust(n)은 n개의 칸 중에서 우측정렬이라는 뜻(https://blackhippo.tistory.com/entry/Python-print%EB%AC%B8-%EC%98%A4%EB%A5%B8%EC%AA%BD%EC%99%BC%EC%AA%BD-%EC%A0%95%EB%A0%AC%ED%95%98%EA%B8%B0-%EC%9D%80%ED%96%89%EB%B2%88%ED%98%B8%ED%91%9C-%ED%91%9C%ED%98%84%ED%95%98%EA%B8%B0)
