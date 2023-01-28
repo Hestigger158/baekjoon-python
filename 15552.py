b701f2ca8e8d052792e05d23f5b8a058651ff818
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-01-28 13:03:56 +0900

    Create 15552.py

---
+++ 15552.py
@@ -0,0 +1,18 @@
+# BaekJoon-python-code
+learn baekjoon python by my self.
+
+
+#은 수식 설명
+
+import sys
+t = int(input())
+for i in range(t):
+    a, b = map(int, sys.stdin.readline().split()) # 입력받은 두 수를 가로로 출력하는 식
+    print(a+b)
+    
+ #a, b를 사용하지 않고 바로 푸는 방식 
+ 
+    import sys
+t = int(input())
+for i in range(t):
+    print(sum(map(int, sys.stdin.readline().split())))
