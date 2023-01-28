c0cd640a4683c1ce0c2d85bcba614e083a05d65b
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-01-28 16:49:21 +0900

    Create 10807.py

---
+++ 10807.py
@@ -0,0 +1,9 @@
+# baekjoon-python
+learn baekjoon python by my self.
+
+#은 수식 설명
+
+n = int(input())
+l = list(map(int, input().split())) #2번째 줄의 숫자들을 리스트화
+a = int(input()) #3번째 줄=찾고자 하는 값
+print(l.count(a)) #count안에 a의 값을 넣음으로서, 리스트에서 a의 값의 갯수 만큼 출력
