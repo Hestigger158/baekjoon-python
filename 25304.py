c2151b0aba7fc4c9181aff255a4d1de8494ce14e
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-01-28 13:01:22 +0900

    Create 25304.py

---
+++ 25304.py
@@ -0,0 +1,16 @@
+# BaekJoon-python-code
+learn baekjoon python by my self.
+
+#은 수식 설명입니다
+
+n = int(input())
+t = int(input())
+result = 0
+for i in range(t):
+    a, b = map(int, input().split())
+    result += a*b # a*b의 값을 계속 더함 
+    
+if result == n:
+    print("Yes")
+else:
+    print("No")
