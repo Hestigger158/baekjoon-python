410c50b5f28cd1bc0978cec380265411587bd76a
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-01-30 22:06:09 +0900

    Update 10818.py

--- 10818.py
+++ 10818.py
@@ -3,8 +3,8 @@ learn baekjoon python by my self.
 
 #은 수식설명
 
-z = int(input()) #주어진 숫자 처리용
-l = list(map(int, input().split())) # 2번째 줄의 모든 수를 리스트화
-a = min(l) #리스트에 있는 숫자(int)중 작은 수 추출
-b = max(l) #리스트에 있는 숫자(int)중 큰 수 추출
-print(a, b) #(작은수) (큰수)로 표출
+z = int(input())
+l = list(map(int, input().split()))              #입력된 두번째 줄(z로 첫줄을 처리함) 수를 리스트화
+a = min(l)                                       #리스트에 있는 숫자(int)중 작은 수
+b = max(l)                                       #리스트에 있는 숫자(int)중 큰 수
+print(a, b)
