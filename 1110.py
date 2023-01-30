361e67c04c6c19f4a1d1b02ec26d995ff995cfef
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-01-30 22:11:04 +0900

    Update 1110.py

--- 1110.py
+++ 1110.py
@@ -5,14 +5,14 @@ learn baekjoon python by my self.
 
 n = int(input())
 t = n
-p = 0 # 카운트 횟수
+p = 0                              # 카운트 횟수
 
 while 1:
-    a = n // 10 # n의 앞자리 수
-    b = n % 10 # n의 뒷자리 수
+    a = n // 10                    # n의 앞자리 수
+    b = n % 10                     # n의 뒷자리 수
     c = (b * 10) + (a + b) % 10 
-    n = c # 새로 n의 값을 생성
-    p += 1 # 카운트 횟수 증가
+    n = c                          # 새로 n의 값을 생성
+    p += 1                         # 카운트 횟수 증가
     if c == t: 
      break
 print(p)
