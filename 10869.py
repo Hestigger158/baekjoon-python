c667c5684143002592871bb319227abdc539b8e0
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-01-30 22:08:14 +0900

    Update 10869.py

--- 10869.py
+++ 10869.py
@@ -1,8 +1,7 @@
  # baekjoon no.10869 website: https://www.acmicpc.net/problem/10869
  
-
-
+#은 수식 설명
 
 a, b = map(int, input().split())
-for op in ['+', '-', '*', r'//', '%']:
-  print(eval(f'{a}{op}{b}'))
+for op in ['+', '-', '*', r'//', '%']:          #각 사칙연산의 기호를 리스트화
+  print(eval(f'{a}{op}{b}'))                    #f'string을 활용해 주어진 a, b값과 위 리스트(사칙연산기호)가 반복되게 설정 후 eval로 계산하게 함
