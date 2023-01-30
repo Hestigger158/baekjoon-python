1e18c5cdbf37cd8a175ce9596b0595408cae6d11
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-01-30 22:09:18 +0900

    Update 10871.py

--- 10871.py
+++ 10871.py
@@ -4,7 +4,7 @@ learn baekjoon python by my self.
 #은 수식 설명
 
 a, b = map(int, input().split())
-c = list(map(int,input().split())) #주어진 수열을 리스트화
+c = list(map(int,input().split()))                    #주어진 수열을 리스트화
 for d in range(a):
-    if c[d] < b: # 각 c[0~(a-1)]의 값과 b의 값을 비교
-        print(c[d], end=" ") #end=" "를 넣음으로써 가로로 출력하게 만듬
+    if c[d] < b:                                      # 각 c[0~(a-1)]의 값과 b의 값을 비교
+        print(c[d], end=" ")                          #end=" "를 넣음으로써 가로로 출력하게 만듬
