bd3eece07a7670e793563d663effe4a15c637435
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-02-03 01:43:45 +0900

    Update 8958.py

--- 8958.py
+++ 8958.py
@@ -17,3 +17,18 @@ for i in range(a):
         else:                # 변수 t의 값이 'X'일 때
             c = 0            # c의 값을 0으로 초기화
     print(b)
+
+# 이지버전(이해한 식)
+
+n = int(input())                
+for i in range(n):
+    l = list(input())           # 주어진 O,X를 리스트화
+    a = 0                       
+    for i in range(len(l)):    
+        while 0 <= i:           # 리스트 l의 갯수만큼 반복
+            if l[i] == 'O':     # l[i]가 O일 때
+                a += 1          # a의 값을 더함
+                i -= 1          # i의 값을 뺌(i의 값을 내림으로써, 'while 0 <= i'를 반복을 멈추게 함)
+            else:               # l[i]가 X일 때
+                break
+    print(a)
