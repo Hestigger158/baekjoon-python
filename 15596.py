c9d11b93dba21ad6b68ffc8ca9f42734479cee04
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-02-04 00:27:02 +0900

    Update 15596.py

--- 15596.py
+++ 15596.py
@@ -4,4 +4,11 @@ learn baekjoon python by my self.
 #은 수식 설명
 
 def solve(a):         # 기존에 주어진 식
-    return sum(a)     # a의 합을 구하면 되므로 
+    return sum(a)     # a의 합을 구하면 되므로
+
+# 다른 방식
+def solve(a):        # 기존에 주어진 식
+    b = 0            # 답을 추출하기 위한 문자
+    for i in a:      
+        b += i       # i 는 a의 모든 수가 될 때까지 b에 i를 더함
+    return b
