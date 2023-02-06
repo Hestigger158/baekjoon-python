05cbeb0165e93cdf5a447aa97b0eb9914b49b924
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-02-06 20:49:37 +0900

    Create 11654.py

---
+++ 11654.py
@@ -0,0 +1,15 @@
+# baekjoon-python
+learn baekjoon python by my self.
+
+#은 수식 설명
+
+a = input()
+b = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"     # 대문자를 나열
+if '0' <= a <= '9':                  # a가 숫자일 경우
+    print(int(a) + 48)
+elif "A" <= a <= "Z":                # a가 대문자일 경우
+    c = b.index(a)                   # b에서 몇번째 수인지 추출
+    print(int(c) + 65)
+else:                                # 나머지, 소문자일 경우
+    d = b.lower().index(a)           # 나열된 값을 소문자로 변환 후 몇번째 수인지 추출
+    print(int(d) + 97)
