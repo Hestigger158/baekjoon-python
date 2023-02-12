a693373d914a703b0d3097210a2a4e0a7441a745
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-02-12 15:26:10 +0900

    Update 1152.py

--- 1152.py
+++ 1152.py
@@ -3,5 +3,8 @@ learn baekjoon python by my self.
 
 #은 수식 설명
 
-a = list(input().split())            # 주어진 문자열을 공백을 기준으로 리스트화
-print(len(a))                  # 리스트의 갯수를 추출
+a = input()                    
+b = list(a.split())            # 주어진 문자열을 공백을 기준으로 리스트화
+print(len(b))                  # 리스트의 갯수를 추출
+
+print(len(list(input().split())))    # 한 줄로 요약
