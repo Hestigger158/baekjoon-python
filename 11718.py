496587f619f303d5807f990f87f5848ab37e8ff8
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-03-03 02:04:58 +0900

    Update 11718.py

--- 11718.py
+++ 11718.py
@@ -5,7 +5,7 @@ baek-joon python learn and study my self
 
 import sys                                     
 while 1:                                       # 무한 반복(1 또는 True 사용하면 됨 true 아니고 True)
-	a=str(sys.stdin.readline().strip())          # 단어가 2개이상일 경우 가로로 출력하도록 입력 조정
+	a=str(sys.stdin.readline().strip())          # 단어가 2개이상일 경우 가로로 출력하도록 입력 조정 + .strip()으로 '/n'의 값을 삭제하기
 	if len(a)==0:                                # 주어진 단어의 갯수가 0일 때, 즉 주어진 값이 더 이상 존재하지 않을 때 멈춤
 		break
 	print(a)
