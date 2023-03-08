cb40fbcb4e91c623e312b8d5141fd8d3a9e463e0
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-03-08 20:38:09 +0900

    Create 2501.py

---
+++ 2501.py
@@ -0,0 +1,17 @@
+# baekjoon-python
+baek-joon python learn and study my self
+
+#은 수식 설명
+
+n, k = map(int, input().split())
+a = []                                    # 약수를 집어넣기 위한 빈 리스트
+for i in range(1,n+1):                    # 1부터 n까지 반복문
+    b = n%i                               # n(주어진 수)를 i로 나눴을 때 나머지
+    if b == 0:                            # 나머지가 0일 때 리스트 a에 나머지가 0이되는 i값을 추가
+        a.append(i)
+    else:                                
+        pass
+if len(a) < k:                            # (리스트a의 원소갯수) < (주어진 수) 일때는 0을 추출
+    print('0')
+else:                                     # 리스트에서 k-1번째 원소를 추출(리스트는 0부터 추출)
+    print(a[k-1])
