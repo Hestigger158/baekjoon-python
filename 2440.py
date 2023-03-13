eff18fb69677e8da38e7c452cedfda40b38dea41
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-03-13 21:31:37 +0900

    Create 2440.py

---
+++ 2440.py
@@ -0,0 +1,10 @@
+# baekjoon-python
+baek-joon python learn and study my self
+
+#은 수식 설명
+
+n = int(input())
+s = n              # input값과 같지만 다른 변수로 설정
+while s > 0:       # s가 0보다 클 때까지 반복
+    print('*'*s)   # *을 s만큼 생성
+    s -= 1
