33847edb92aa3d97bfb50dbcec0d8625f61de8ab
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-03-13 21:35:06 +0900

    Create 2441.py

---
+++ 2441.py
@@ -0,0 +1,10 @@
+# baekjoon-python
+baek-joon python learn and study my self
+
+#은 수식 설명
+
+n = int(input())
+s = n
+while s > 0:
+    print(str('*'*s).rjust(n))     # n개의 칸에서 *을 s개만큼 출력하되, 우측정렬로 출력
+    s -= 1
