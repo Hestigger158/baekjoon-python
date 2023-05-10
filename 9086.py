7f5f9655cf6ddbdccb8dacbaef4c8b6fb8b2427f
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 23:35:08 +0900

    문자열

--- 9086.py
+++ 9086.py
@@ -4,6 +4,6 @@ learn baekjoon python by my self.(백준 독학 저장소)
 #은 수식 설명
 
 n = int(input())
-for i in range(n):         # n개만큼 반복
+for i in range(n):         # n번 반복
     a = str(input())       # 문자열 입력
     print(a[0]+a[-1])      # 그 문자의 첫번째([0])와 마지막([-1])을 입력하되 빈칸 없이(+) 출력
