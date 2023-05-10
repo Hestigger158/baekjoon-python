f02fb03b8338adc5a0a2d931833ef74921c1f61a
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 16:37:10 +0900

    알파벳 찾기

--- 10809.py
+++ 10809.py
@@ -6,6 +6,6 @@ a = input()
 b = 'abcdefghijklmnopqrstuvwxyz'       
 for i in b:                           
     if i in a:
-        print(a.index(i), end=' ')    # a에 있는 알파벳의 순서를 가로로 나열
+        print(a.index(i), end=' ')    # a에 있는 알파벳의 순서를 가로(end=' ')로 공백을 포함해 나열
     else:
-        print(-1, end=' ')
+        print(-1, end=' ')            # 포함 되어 있지 않는 경우 '-1' 출
