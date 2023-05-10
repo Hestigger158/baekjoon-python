857c7f99a51fcf23c1f261d0d308e56f3e54696e
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 17:16:29 +0900

    정수 N개의 합

--- 15596.py
+++ 15596.py
@@ -4,7 +4,7 @@ learn baekjoon python by my self.
 #은 수식 설명
 
 def solve(a):         # 기존에 주어진 식
-    return sum(a)     # a의 합을 구하면 되므로
+    return sum(a)     # a의 합을 구함
 
 # 다른 방식
 def solve(a):        # 기존에 주어진 식
