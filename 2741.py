e8315a2fb015b737475ddce1bb1738470fc3cbe8
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 23:19:20 +0900

    N 찍기

--- 2741.py
+++ 2741.py
@@ -3,6 +3,6 @@ baek-joon python learn and study my self
 
 #은 수식 설명
 
-n = int(input())          # 주어진 입력값(input) 변환
-for i in range(1, n+1):   # 1부터 n(주어진 값)까지 반복
-    print(i)              # 1부터 출력
+n = int(input())           # 주어진 입력값(input) 변환
+for i in range(1, n+1):    # 1부터 n(주어진 값)까지 반복
+    print(i)               # 1부터 출력
