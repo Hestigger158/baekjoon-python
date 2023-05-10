8d0a5b1472d7631c6503c7a13b7d7a66a64d9779
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 16:48:18 +0900

    X보다 작은 수

--- 10871.py
+++ 10871.py
@@ -6,5 +6,5 @@ learn baekjoon python by my self.
 a, b = map(int, input().split())
 c = list(map(int,input().split()))                    #주어진 수열을 리스트화
 for d in range(a):
-    if c[d] < b:                                      # 각 c[0~(a-1)]의 값과 b의 값을 비교
+    if c[d] < b:                                      # 각 리스트c[0~(a-1)]의 값과 b의 값을 비교
         print(c[d], end=" ")                          #end=" "를 넣음으로써 가로로 출력하게 만듬
