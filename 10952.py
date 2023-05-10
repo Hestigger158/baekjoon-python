919c266f922bd3699421817bfd9b610f4f49a70b
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 16:51:16 +0900

    A+B - 5

--- 10952.py
+++ 10952.py
@@ -6,7 +6,7 @@ learn baekjoon python by my self.
 import sys
 for i in sys.stdin:
     c = sum(map(int, i.split()))
-    if c > 0:
+    if c > 0:                     # 두개의 합이 0보다 클 때
         print(c)
-    else:
+    else:                         # 마지막으로 0 0이 들어오기에 다른 경우 실행
         break
