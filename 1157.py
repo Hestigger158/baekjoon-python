e568c50983729793a64db8f0c467657de4e628d1
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 17:01:18 +0900

    단어 공부

--- 1157.py
+++ 1157.py
@@ -12,4 +12,4 @@ for i in a:
 if b.count(max(b)) >= 2:                # 리스트 b에서 가장 큰 수의 갯수가 2 이상일 경우
     print('?')
 else:
-    print(a[(b.index(max(b)))])         # 가장 많은 갯수의 알파벳을 추출
+    print(a[(b.index(max(b)))])         # 가장 많은 갯수의 알파벳을 출력
