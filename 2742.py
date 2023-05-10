db62667ed7522304408aca79d2c61d0b686e94e6
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 23:20:41 +0900

    기찍 N

--- 2742.py
+++ 2742.py
@@ -4,7 +4,7 @@ baek-joon python learn and study my self
 #은 수식 설명
 
 n = int(input())
-s = n                # input값과 같은 값이지만 다른 문자로도 설정
+s = n                # input값과 같은 값이지만 다른 문자로 설정
 while s > 0:         # s가 0보다 클 때까지 반복
     print(s)         # s를 출력
     s -=1            # s를 1만큼 감소
