e5d2f55f71ede7b39abf34c869de82870a9ee44d
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 23:28:34 +0900

    크로아티아 알파벳

--- 2941.py
+++ 2941.py
@@ -8,6 +8,6 @@ b = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']     # 크로아티아 알�
 c = 0
 for i in b:                                               
     while i in a:                                         # i(리스트 b의 요소)가 a(주어진 문자열)에 있을 때
-        c += a.count(i)                                   # a에 속한 i의 갯수만큼 추가
+        c += a.count(i)                                   # a에 속한 i의 개수를 추가
         a = a.replace(i,'0'*len(i))                       # a에 있는 i를 다른문자('0')로 교체
 print(c + len(a)-a.count('0'))                            # c(a에 속한 b의 수) + (a의 문자 수) - (a에 있는 문자'0'의 수)
