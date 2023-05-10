966143d8b797f6a0a4df3e3a4202b51e184d4bdd
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 17:18:21 +0900

    평균

--- 1546.py
+++ 1546.py
@@ -4,5 +4,5 @@ learn baekjoon python by my self.
 #은 수식 설명
 
 n = int(input())                        
-a = list(map(int, input().split()))          #두번째 주어진 수를 리스트 화
+a = list(map(int, input().split()))          #두번째 주어진 수를 리스트에 삽입
 print(sum(a) / max(a) * 100 / n)
