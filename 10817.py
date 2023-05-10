97134a323617fbcc40f966109620553a5a890bdf
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 16:44:17 +0900

    세 수

--- 10817.py
+++ 10817.py
@@ -10,3 +10,13 @@ s.append(b)
 s.append(c)
 s.sort()         # 리스트 s  오름차순으로 정렬
 print(s[1])
+
+#다른 방식
+a, b, c = map(int, input().split())
+s = []
+s.append(a)      # 리스트 s에 a, b, c 값 추가
+s.append(b)
+s.append(c)
+s.remove(max(s))  # 리스트에서 중간 값이므로 최댓값 최솟값을 리스트에서 삭제
+s.remove(min(s))
+print(s[0])
