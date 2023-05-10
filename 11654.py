3aa379b678a902d1c439526139c99f77affebd4e
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 17:02:47 +0900

    아스키 코드

--- 11654.py
+++ 11654.py
@@ -6,10 +6,10 @@ learn baekjoon python by my self.
 a = input()
 b = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"     # 대문자를 나열
 if '0' <= a <= '9':                  # a가 숫자일 경우
-    print(int(a) + 48)
+    print(int(a) + 48)               # 아스키 코드에 맞는 수를 출력
 elif "A" <= a <= "Z":                # a가 대문자일 경우
-    c = b.index(a)                   # b에서 몇번째 수인지 추출
+    c = b.index(a)                   # b에서 몇번째 수인지 출력
     print(int(c) + 65)
 else:                                # 나머지, 소문자일 경우
-    d = b.lower().index(a)           # 나열된 값을 소문자로 변환 후 몇번째 수인지 추출
+    d = b.lower().index(a)           # 나열된 값을 소문자로 변환 후 몇번째 수인지 출력
     print(int(d) + 97)
