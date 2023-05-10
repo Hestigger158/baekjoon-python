446e1e9cab7a7ff6261dfde8377117c887336784
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 16:45:00 +0900

    최소, 최대

--- 10818.py
+++ 10818.py
@@ -5,6 +5,6 @@ learn baekjoon python by my self.
 
 z = int(input())
 l = list(map(int, input().split()))              #입력된 두번째 줄(z로 첫줄을 처리함) 수를 리스트화
-a = min(l)                                       #리스트에 있는 숫자(int)중 작은 수
-b = max(l)                                       #리스트에 있는 숫자(int)중 큰 수
+a = min(l)                                       #리스트에 있는 숫자(int)중 최솟값
+b = max(l)                                       #리스트에 있는 숫자(int)중 최댓값
 print(a, b)
