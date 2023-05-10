79a2c756f8be982459665e9f6ccb73aa6c464afd
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 17:36:00 +0900

    별 찍기 - 7

--- 2444.py
+++ 2444.py
@@ -7,7 +7,7 @@ n = int(input())
 for i in range(n):
     a = "*"*((i+1)*2-1)         # 맨 위부터 나열 될 *의 갯수
     print(a.rjust(n+i))         # 우측정렬로 좌측에만 빈 공간을 나열
-j = n
+j = n                           # 주어진 수와 같은 값 생성
 while j >=2:
     c = "*"*((j-1)*2-1)         # n+1번째 줄부터 나열 될 *의 갯수
     print(c.rjust(n+j-2))       # 우측 정렬로 좌측에만 빈 공간을 나열
