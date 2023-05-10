911db02bcdba76cc25d431981a57e0e9829b15b4
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 17:29:44 +0900

    별 찍기 - 1

--- 2438.py
+++ 2438.py
@@ -5,4 +5,8 @@ learn baekjoon python by my self.
 
 n = int(input())
 for a in range(n):
-    print('*' *(a+1))
+    print('*' *(a+1))   # a=0부터 n-1까지이므로 a+1을 해줌
+    
+# a를 1부터 n까지 출력하고 싶다면
+for a in range(1,n+1):
+    print('*'*a)
