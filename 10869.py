3e15bbdf836e6be6f710456da39d1d643feea034
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 16:47:38 +0900

    사칙연산

--- 10869.py
+++ 10869.py
@@ -2,6 +2,12 @@
  
 #은 수식 설명
 
+
+a, b = map(int, input().split())
+print(a+b, a-b, a*b, a//b, a%b)                 #그냥 다 표현
+
+#다른 방법
 a, b = map(int, input().split())
 for op in ['+', '-', '*', r'//', '%']:          #각 사칙연산의 기호를 리스트화
-  print(eval(f'{a}{op}{b}'))                    #f'string을 활용해 주어진 a, b값과 위 리스트(사칙연산기호)가 반복되게 설정 후 eval로 계산하게 함
+  print(eval(f'{a}{op}{b}'))               
+#f'string을 활용해 주어진 a, b값과 위 리스트(사칙연산기호)가 반복되게 설정 후 eval로 계산하게 함
