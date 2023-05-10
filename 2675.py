458bb152fe07f75c6fae1f19c3ff9dfb7236dfed
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 17:55:37 +0900

    문자열 반복

--- 2675.py
+++ 2675.py
@@ -5,9 +5,9 @@ learn baekjoon python by my self.
 
 n = int(input())
 for i in range(n):
-    a, b = input().split()         # a(숫자), b(문자열)로 지정
-    c = ""                         # 답을 나열할 빈 항목
-    for l in b:                    # b에서 각 항목 추출
+    a, b = input().split()          # a(숫자), b(문자열)로 지정
+    c = ""                          # 답을 나열할 빈 항목
+    for l in b:                     # b에서 각 항목 추출
         for i in range(int(a)):    
-            c += l                 # 빈 항목에 추출한 항목을 a만큼 더함
+            c += l                  # 빈 항목에 추출한 항목을 a만큼 더함
     print(c)
