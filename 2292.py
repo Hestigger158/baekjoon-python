780a66330b20be64f93b2652fcda38d2433b7268
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 17:27:40 +0900

    벌집

--- 2292.py
+++ 2292.py
@@ -7,7 +7,7 @@ learn baekjoon python by my self.(백준 독학 저장소)
 n = int(input())
 a = 1                    # 지나갈 방의 갯수
 b = 6                    # 주어진 규칙으로 거처야할 방의 갯수가 늘어날 때마다 방의 갯수가 6개씩 늘어남
-c = 1                    # 최초의 방 1개
+c = 1                    # 최초의 방
 while n > c:             
   a += 1                 # 방의 갯수가 늘어남
   c += b                 # 방의 갯수를 더함
