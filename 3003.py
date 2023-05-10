2b0e9fdd09fbff6edee53e3090a294c29d5ccf3f
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-05-10 23:29:56 +0900

    킹, 퀸, 룩, 비숍, 나이트, 폰

--- 3003.py
+++ 3003.py
@@ -1,7 +1,7 @@
 # BaekJoon-python-code
 learn baekjoon python by my self.
 
+#은 수식 설명
 
-
-king, queen, rook, bishop, knight, pawn=map(int,input().split())
-print(1-king, 1-queen, 2-rook, 2-bishop, 2-knight, 8-pawn)
+king, queen, rook, bishop, knight, pawn=map(int,input().split())  # 각 숫자를 체스말에 대입
+print(1-king, 1-queen, 2-rook, 2-bishop, 2-knight, 8-pawn)        # 올바른 숫자에서 대입한 숫자를 뺌
