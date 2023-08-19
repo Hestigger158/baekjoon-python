f93235e5b2cc60dbb76d1e85e2f661afcb0bbc0b
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-08-20 01:28:46 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 1

---
+++ 24262.py
@@ -0,0 +1,7 @@
+# baekjoon-python
+baek-joon python learn and study my self
+
+#은 수식 설명
+
+print("""1      # MenOfPassion 알고리즘1의 수행횟수는 무조건 1이다(i=[n/2]이므로 A[i]를 출력하기 때문에 단 한번만 계산 하면 된다) 
+0""")           # 수행횟수는 무조건 1이므로 상수차수인 0을 출력하면 된다
