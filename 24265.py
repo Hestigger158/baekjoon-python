8ed922d3ac40c5dbb0ea17790d47fc1a34cbf24d
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-08-24 18:34:51 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 4

---
+++ 24265.py
@@ -0,0 +1,9 @@
+# baekjoon-python
+baek-joon python learn and study my self
+
+#은 수식 설명
+
+a=int(input())          # 주어진 수를 입력받는다
+b=a*(a-1)//2            # 알고리즘의 수행시간(1부터 a-1까지의 합)을 계산한다
+print(b)                # 수행시간을 출력한다
+print(2)                # 1부터 a-1의 합은 n*(n-1)//2이므로 차수는 2이다
