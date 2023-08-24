e83cd8b690be2cd39782ee64f018273730066765
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-08-24 18:04:37 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ 24623.py
@@ -0,0 +1,8 @@
+# baekjoon-python
+baek-joon python learn and study my self
+
+#은 수식 설명
+
+a=int(input()) # 알고리즘의 수행시간을 알기 위해 입력한 수를 받는다
+print(a)       # 그 수만큼 for문이 돌기 때문에 수행시간이 입력한 숫자이다
+print(1)       # x를 입력하면 x번 수행 하므로 1차식이기에 1을 출력함
