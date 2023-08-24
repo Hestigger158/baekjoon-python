0bf1bdd1c0471ca7cdad13dcb8dcff61739920bc
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-08-24 19:35:16 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 6

---
+++ 24267.py
@@ -0,0 +1,9 @@
+# baekjoon-python
+baek-joon python learn and study my self
+
+#은 수식 설명
+
+a=int(input())       # 주어진 수를 입력
+b=a*(a-1)*(a-2)//6   # 수행횟수를 계산한 결과
+print(b)             # 수행횟수를 출력
+print(3)             # 위의 식의 최대차수는 3차수이다
