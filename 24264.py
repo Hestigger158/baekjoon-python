befb871336e5d87f2bdf16d9bff46d54dd8c79ad
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-08-24 18:07:09 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 3

---
+++ 24264.py
@@ -0,0 +1,8 @@
+# baekjoon-python
+baek-joon python learn and study my self
+
+#은 수식 설명
+
+a=int(input())        # 주어진 수를 받는다
+print(a**2)           # for문안에 for문이 실행 되므로 실행횟수는 a의 제곱만큼이다
+print(2)              # x를 입력하면 x의 제곱만큼 실행되므로 차수는 2차수이다
