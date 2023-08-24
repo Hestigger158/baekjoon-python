8bff0209cd7100d42f2611968fbaf8e423a2105e
Hestigger158 <123177112+Hestigger158@users.noreply.github.com>
2023-08-24 19:21:26 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 5

---
+++ 26266.py
@@ -0,0 +1,8 @@
+# baekjoon-python
+baek-joon python learn and study my self
+
+#은 수식 설명
+
+a=int(input())      # 주어진 수를 입력 받음
+print(a**3)         # for안에 for안에 for문이므로 세제곱만큼 반복 됨
+print(3)            # 세제곱만큼 반복이므로 최고차수는 3차수이다
